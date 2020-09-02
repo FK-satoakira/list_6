73f3cfb0621fbe88da4a21670750c2e539c29b84
FK-satoakira <69459763+FK-satoakira@users.noreply.github.com>
2020-09-03 02:41:43 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,18 @@
 package list05_practice;
-import java.util.function.IntBinaryOperator;
 
 public class Main {
 
-	public static int sub(int a, int b) {
-		return a-b;
+	public static String sub(int a, int b) {
+		return a+"+"+b+"="+(a+b);
 	}
 
 	public static void main(String[] args) {
-		IntBinaryOperator tekitou = Main::sub; /*IntBinaryOperatorはapplyAsInt(int, int)を関数メソッドに持つ関数型インタフェース*/
-		int x = 150;						 /*Main::subは関数を変数に入れる専用の記述か...*/
+		SatoFunction tekitou = Main::sub; /*IntBinaryOperatorはapplyAsInt(int, int)を関数メソッドに持つ関数型インタフェース*/
+		int x = 1250;						 /*Main::subは関数を変数に入れる専用の記述か...*/
 		int y = 96;
-		int a = tekitou.applyAsInt(x, y); /*applyAsIntもお決まりのメソッド*/
-		System.out.println(x+"-"+y+"="+a);
+		String a =tekitou.satoS(x, y); /*applyAsIntもお決まりのメソッド*/
+//		System.out.println(x+"-"+y+"="+a);
+		System.out.println(a);
 	}
-
+//自作のSatoFunctionを使うためコードを変更した
 }
