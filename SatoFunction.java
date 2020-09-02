566b5f19af60882f1776d148658c3ecace94e558
FK-satoakira <69459763+FK-satoakira@users.noreply.github.com>
2020-09-03 02:38:51 +0900

    Create SatoFunction.java

---
+++ SatoFunction.java
@@ -0,0 +1,7 @@
+package list05_practice;
+
+public interface SatoFunction {
+	public abstract String satoS(int a, int b); /*abstractメソッドはどちらか一つじゃないと作動しなかった*/
+//	public abstract int satoI(int a, int b);
+
+}
