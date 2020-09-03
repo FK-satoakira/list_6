22635a4a141be3ad065d90f13b0c71eeeba5ecd6
FK-satoakira <69459763+FK-satoakira@users.noreply.github.com>
2020-09-03 22:33:30 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,27 +9,10 @@ class Hero{
 	}
 }
 public class Main {
-	public static String sub(Hero a, String b) {
-		return b+a.getHp()+"の二乗は"+(a.getHp()*a.getHp());
-	}
     public static void main(String[] args) {
-//    	MyFunction func = (int a) -> {return a*a;};
-//    	int x = 200;
-//    	int a = func.call(x);
-//    	System.out.println(x+"の二乗は"+a);
-
     	Hero h = new Hero();
-//    	System.out.println("h.getHp()="+h.getHp());
-//    	MyFunction f = (Hero) -> {return "hpは" + h.getHp() + "です";};
-//    	int a = h.getHp();
-//    	System.out.println(f.call(a));
-    	MyFunction func = Main::sub;
-    	String a = func.call(h,"計算の結果、");
+    	MyFunction func = (Hero a, String b) -> {return b+a.getHp()+"の二乗は"+(a.getHp())*(a.getHp());};	/*(Hero a, String b)のHero引数がここが躓いたところ。*/
+    	String a = func.call(h,"計算の結果は、");
     	System.out.println(a);
-
-//今のところ、ポイントは３つ。
-//12行目、public static String sub(Hero a, String b)のリターン型と引数、
-//27行目、String a = func.call(h,"計算の結果、")のリターン型と引数、
-//MyFunction.javaのString call(Hero a, String b)のリターン型と引数、を揃える
     }
 }
