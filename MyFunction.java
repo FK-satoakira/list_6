10330ed72a216a5d778006f01a146d7aee36665d
FK-satoakira <69459763+FK-satoakira@users.noreply.github.com>
2020-09-03 22:34:10 +0900

    Update MyFunction.java

--- MyFunction.java
+++ MyFunction.java
@@ -1,9 +1,5 @@
 package practice;
 
 public interface MyFunction {
-//	public abstract String call(int a, int b);
-//	public abstract int call(int a, int b);
 	public abstract String call(Hero a, String b);
-
-
 }
