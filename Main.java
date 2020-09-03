00518fb76f3dee60f3ce91310825cd48970e1f87
FK-satoakira <69459763+FK-satoakira@users.noreply.github.com>
2020-09-03 22:58:27 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,23 @@
+// 「スレッド「メイン」の例外java.lang.Error：未解決のコンパイル問題：
+// ラムダ式のパラメーターhは、外側のスコープで定義されている別のローカル変数を再宣言できません。」
+// このコードだとこういうエラーが出てしまう
 package practice;
-class Hero{
-	private int hp = 100;
-	public int getHp() {
-		return hp;
-	}
-	public void setHp(int hp) {
-		this.hp = hp;
+public class Main{
+	public static void main(String[] args) {
+		Hero h = new Hero();
+//		MyFunction func = (h) -> {return h.getHp()+h.getHp();};
+		MyFunction func = (Hero) -> {return h.hp;};　　/*ここを(Hero h)とできない*/
+		int a = func.call(h);
+		System.out.println(a);
 	}
 }
-public class Main {
-    public static void main(String[] args) {
-    	Hero h = new Hero();
-    	MyFunction func = (Hero a, String b) -> {return b+a.getHp()+"の二乗は"+(a.getHp())*(a.getHp());};	/*(Hero a, String b)のHero引数がここが躓いたところ。*/
-    	String a = func.call(h,"計算の結果は、");
-    	System.out.println(a);
-    }
-}
+
+
+
+
+
+
+
+
+
+
