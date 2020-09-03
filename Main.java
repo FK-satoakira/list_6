5b25afc488cc24d722add14c13ce27fadc8d23cb
FK-satoakira <69459763+FK-satoakira@users.noreply.github.com>
2020-09-03 13:36:33 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,18 +1,35 @@
-package list05_practice;
-
+package practice;
+class Hero{
+	private int hp = 100;
+	public int getHp() {
+		return hp;
+	}
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+}
 public class Main {
-
-	public static String sub(int a, int b) {
-		return a+"+"+b+"="+(a+b);
+	public static String sub(Hero a, String b) {
+		return b+a.getHp()+"の二乗は"+(a.getHp()*a.getHp());
 	}
+    public static void main(String[] args) {
+//    	MyFunction func = (int a) -> {return a*a;};
+//    	int x = 200;
+//    	int a = func.call(x);
+//    	System.out.println(x+"の二乗は"+a);
 
-	public static void main(String[] args) {
-		SatoFunction tekitou = Main::sub; /*IntBinaryOperatorはapplyAsInt(int, int)を関数メソッドに持つ関数型インタフェース*/
-		int x = 1250;						 /*Main::subは関数を変数に入れる専用の記述か...*/
-		int y = 96;
-		String a =tekitou.satoS(x, y); /*applyAsIntもお決まりのメソッド*/
-//		System.out.println(x+"-"+y+"="+a);
-		System.out.println(a);
-	}
-//自作のSatoFunctionを使うためコードを変更した
+    	Hero h = new Hero();
+//    	System.out.println("h.getHp()="+h.getHp());
+//    	MyFunction f = (Hero) -> {return "hpは" + h.getHp() + "です";};
+//    	int a = h.getHp();
+//    	System.out.println(f.call(a));
+    	MyFunction func = Main::sub;
+    	String a = func.call(h,"計算の結果、");
+    	System.out.println(a);
+
+//今のところ、ポイントは３つ。
+//12行目、public static String sub(Hero a, String b)のリターン型と引数、
+//27行目、String a = func.call(h,"計算の結果、")のリターン型と引数、
+//MyFunction.javaのString call(Hero a, String b)のリターン型と引数、を揃える
+    }
 }
---
+++ MyFunction.java
@@ -0,0 +1,9 @@
+package practice;
+
+public interface MyFunction {
+//	public abstract String call(int a, int b);
+//	public abstract int call(int a, int b);
+	public abstract String call(Hero a, String b);
+
+
+}
