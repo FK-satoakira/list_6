12927d2b0d55024946b3f6d571143e54d5f5578e
FK-satoakira <69459763+FK-satoakira@users.noreply.github.com>
2020-09-05 01:15:26 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,8 +3,8 @@ package practice;
 public class Main {
     public static void main(String[] args) {
     	FuncList funclist = new FuncList();
-    	Func1 func1 = FuncList::isOdd;
-    	Func2 func2 = funclist::addNamePrefix;
+    	Func1 func1 = FuncList::isOdd;          /*メソッドがstaticなのでクラス名*/
+    	Func2 func2 = funclist::addNamePrefix;  /*メソッドが非staticなのでインスタンス*/
     	System.out.println(func1.call(3));
     	System.out.println(func2.call(func1.call(2), "yamada"));
     }
